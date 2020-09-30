7569740eceebeb24a32f3a40a1f573c84df50768
Pavel <lony2396@gmail.com>
2020-09-30 23:10:56 +0600

    23.10

--- home.js
+++ home.js
@@ -6,13 +6,18 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов', '');
-      b = prompt('На сколько оцените его?', '');
-      c = prompt('Один из последних просмотренных фильмов', '');
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(1);
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов', '');
+          b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+} 
 console.log(personalMovieDB);
+
+
+
+
+
+
+
+
 

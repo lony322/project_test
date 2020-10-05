10203f163c5f524aa13a5c628940154e47e709d1
Emma Paris <eparis@atlassian.com>
2020-10-05 15:16:51 +0600

    15.00

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
+/* const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,13 +7,21 @@ const personalMovieDB = {
     privat: false
 };
 for (let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов', '');
-          b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
-} 
+   // let a = prompt('Один из последних просмотренных фильмов', '');
+    let b = prompt('На сколько оцените его?', '');
+    var nymberSymbol = a.length;     
+    if (nymberSymbol !== "" || nymberSymbol > 50 || nymberSymbol == null || numberOfFilms == undefined){
+       (i >= 1) ? i-- : i = i;
+          personalMovieDB.movies[a] = b;
+    }
+        } 
 console.log(personalMovieDB);
 
+function lastFIlm (a){
+    String a = prompt('Один из последних просмотренных фильмов', '');
+}
 
+ */
 
 
 
---
+++ test.js
@@ -0,0 +1,19 @@
+let numberOfFilms = 0; //Сколько всего фильмов
+let lastFilm = ''; //Последний просмотренный фильм
+let filmScore = 0; // Оценнка последнего просмотренного фильма
+let askLastFilm = '';//Переменная для вопроса о последнем фильме
+let askFilmScore = 0;//Переменная для вопроса об оценки пследнего фильма
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+function whatMovie () {
+    return prompt('Какой последнйи фильм ты посмотрел?', '');
+    }
+function whatScore () {
+    return prompt('Какую оценку ты дашь фильму?', '');
+    }
+    personalMovieDB.movies[whatMovie()] =  whatScore();

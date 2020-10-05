6bac965d5b6cfad231a97213af0c95be770ecaaf
Pavel <lony2396@gmail.com>
2020-10-05 20:48:46 +0600

    ada;

--- test.js
+++ test.js
@@ -8,16 +8,17 @@ const personalMovieDB = {
 };
 
 function whatMovie() {
-    let askLastFilm = ""; //Переменная для вопроса о последнем фильме
-    while ((askLastFilm || "").length > 50 || askLastFilm === null || askLastFilm.length === 0) {
+    let askLastFilm; //Переменная для вопроса о последнем фильме
+    while ((askLastFilm || "").length > 50 || askLastFilm === null || (askLastFilm || "").length === 0 || askLastFilm === undefined) {
         askLastFilm = prompt('Какой последнйи фильм ты посмотрел?', '');
     }
 
     let askFilmScore;
     while (!askFilmScore || askFilmScore < 0 || askFilmScore > 10) {
-        /* askFilmScore = prompt('Какую оценку ты дашь фильму?', ''); */
-        askFilmScore = parseFloat(prompt('Какую оценку ты дашь фильму?', ''));
-    }
+           askFilmScore = parseFloat(prompt('Какую оценку ты дашь фильму?', ''));
+           console.log(askFilmScore);
+        }
+
     
     personalMovieDB.movies[askLastFilm] = askFilmScore;
 }
